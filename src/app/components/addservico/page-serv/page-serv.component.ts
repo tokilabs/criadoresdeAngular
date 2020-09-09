2f0aeedbddc719c4aec3e2c2002776b52117b259
AmoreiraT <thiago.m.hq@gmail.com>
2020-09-09 02:01:23 -0300

    up

--- src/app/components/addservico/page-serv/page-serv.component.ts
+++ src/app/components/addservico/page-serv/page-serv.component.ts
@@ -1,3 +1,4 @@
+import { ServService } from './../../../services/serv.service';
 import { Serv } from './../../../models/Service';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Component, OnInit } from '@angular/core';
@@ -18,40 +19,45 @@ export class PageServComponent implements OnInit {
 
 
 
-  constructor(public afs: AngularFireStorage) {
-
+  constructor(public afs: AngularFireStorage, private servServ: ServService) {
+    this.servs = [];
   }
 
   ngOnInit(): void {
-    this.getAll();
+
+    this.servServ.fireGet(this.serv, this.servs);
+
   }
 
-  async getAll() {
-    const firestore = firebase.firestore();
-    const ref = firestore.collection('addserv');
-    const snapshot = await ref.get();
-    console.log(snapshot.size);
-    if (snapshot.size === 0) {
-      console.log("null");
-      alert("Nenhum Documento");
-    } else {
-      snapshot.forEach(doc => {
-        if (doc.exists) {
-          var docId = doc.id;
-          var docData = doc.data();
-          console.log(docData);
-          console.log(docId);
-          this.serv = docData as Serv;
-          console.log(this.serv);
+  // async getAll() {
+  //   const firestore = firebase.firestore();
+  //   const ref = firestore.collection('addserv');
+  //   const snapshot = await ref.get();
+  //   console.log(snapshot.size);
+  //   if (snapshot.size === 0) {
+  //     console.log("null");
+  //     alert("Nenhum Documento");
+  //   } else {
+  //     snapshot.forEach(doc => {
+  //       if (doc.exists) {
+  //         var docId = doc.id;
+  //         var docData = doc.data();
+  //         console.log(docData);
+  //         console.log(docId);
+  //         this.serv = docData as Serv;
+  //         this.servs.unshift(this.serv);
+  //         console.log(this.serv);
+  //         console.log(this.servs);
 
-          // this.addnaPage(this.serv);
-        } else {
-          console.log("null");
-          alert("Nenhum Documento");
-        }
-      });
-    }
-  }
+
+  //         // this.addnaPage(this.serv);
+  //       } else {
+  //         console.log("null");
+  //         alert("Nenhum Documento");
+  //       }
+  //     });
+  //   }
+  // }
 
 
 }
--- src/app/services/serv.service.ts
+++ src/app/services/serv.service.ts
@@ -5,6 +5,8 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Serv } from './../models/Service';
 import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,7 @@ import { Injectable } from '@angular/core';
 export class ServService {
 
   // servUrl: string = 'https://jsonplaceholder.typicode.com/Servs';
+  serv: Serv;
 
   servs: Serv[];
   serv$: Observable<Serv>;
@@ -36,6 +39,36 @@ export class ServService {
     );
   }
 
+  async fireGet(serv: Serv, servs: Serv[]) {
+    const firestore = firebase.firestore();
+    const ref = firestore.collection('addserv');
+    const snapshot = await ref.get();
+    console.log(snapshot.size);
+    if (snapshot.size === 0) {
+      console.log("null");
+      alert("Nenhum Documento");
+    } else {
+      snapshot.forEach(doc => {
+        if (doc.exists) {
+          var docId = doc.id;
+          var docData = doc.data();
+          console.log(docData);
+          console.log(docId);
+          serv = docData as Serv;
+          servs.unshift(serv);
+          console.log(serv);
+          console.log(servs);
+
+
+          // this.addnaPage(this.serv);
+        } else {
+          console.log("null");
+          alert("Nenhum Documento");
+        }
+      });
+    }
+  }
+
   getServs(): Observable<Serv[]> {
 
     if (localStorage.getItem('servs') === null) {
@@ -44,9 +77,11 @@ export class ServService {
       this.servs = JSON.parse(localStorage.getItem('servs'));
     }
     console.log(this.servs);
+
     return of(this.servs.sort((a, b) => {
       return b.id = a.id;
     }));
+
 
   }
 
