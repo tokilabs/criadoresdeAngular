873da7e72ca663daa7d3d31acc979b40578ce985
AmoreiraT <thiago.m.hq@gmail.com>
2020-09-11 13:19:32 -0300

    up

--- src/app/components/precos/precos-contain/precos-contain.component.ts
+++ src/app/components/precos/precos-contain/precos-contain.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-precos-contain',
   templateUrl: './precos-contain.component.html',
-  styleUrls: ['./precos-contain.component.css']
+  styleUrls: ['./precos-contain.component.css'],
 })
 export class PrecosContainComponent implements OnInit {
 
