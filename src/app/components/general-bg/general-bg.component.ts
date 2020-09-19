965314498c02f81ea940aeb3a03101cced6516c5
AmoreiraT <thiago.m.hq@gmail.com>
2020-09-19 04:22:38 -0300

    up

--- src/app/components/general-bg/general-bg.component.ts
+++ src/app/components/general-bg/general-bg.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-general-bg',
   templateUrl: './general-bg.component.html',
-  styleUrls: ['./general-bg.component.css', './../../index/index.component.css']
+  styleUrls: ['./general-bg.component.css']
 })
 export class GeneralBgComponent implements OnInit {
 
