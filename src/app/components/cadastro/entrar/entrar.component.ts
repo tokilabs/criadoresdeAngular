0d24c6f82fdcda885400796ca047155bcfe376ec
AmoreiraT <thiago.m.hq@gmail.com>
2020-10-05 22:16:00 -0300

    up

--- src/app/components/cadastro/entrar/entrar.component.ts
+++ src/app/components/cadastro/entrar/entrar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  showLg: boolean;
+  userOn: boolean;
+
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    var currUser = firebase.auth().currentUser;
+    console.log(currUser);
+
+    if (currUser === null) {
+      this.userOn = false;
+    } else { this.userOn = true; }
+
+    console.log(this.userOn);
   }
 
 }
